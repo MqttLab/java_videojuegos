8a7eb21bf1930ebbe6ff6ef7c2edee208dddd7c0
Mauricio Maldonado <134096302+MqttLab@users.noreply.github.com>
2024-09-27 12:05:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +13,11 @@ public class Main {
                 case 2: mostrarVideojuegos(videoJuegos);
                     break;
                 case 3:
+                    if(modificarVideojuego(videoJuegos)){
+                        System.out.println("Videojuego actualizado con éxito!");
+                    } else {
+                        System.out.println("Videojuego no encontrado...");
+                    }
                     break;
                 case 4:
                     break;
@@ -27,13 +30,13 @@ public class Main {
     static public int menu(){
         Scanner elegir = new Scanner(System.in);
         int sel;
-        System.out.println("##### Ejercicio Videojuegos #####\n" +
+        System.out.print("##### Ejercicio Videojuegos #####\n" +
                 "1. Crear arraylist del tipo Videojuego\n" +
                 "2. Mostrar arraylist\n" +
-                "3. Modificar juego (busqueda por codigo)\n" +
+                "3. Modificar videojuego (busqueda por codigo)\n" +
                 "4. Mostrar videojuegos de consola Nintendo 64\n" +
-                "5. Salir");
-        System.out.print("?: ");
+                "5. Salir\n" +
+                "?: ");
         sel = elegir.nextInt();
         return sel;
     }
@@ -45,8 +48,9 @@ public class Main {
         for(int i=0; i<n; i++){
             videojuegos.add(crearVideojuego());
         }
+        //elegir.close();
     }
-
+    // Opcion 1
     static public Videojuego crearVideojuego(){
         Scanner cons = new Scanner(System.in);
         System.out.print("Ingrese el codigo: ");
@@ -61,15 +65,50 @@ public class Main {
         System.out.print("Ingrese la categoria: ");
         String categoria = cons.next();
         cons.nextLine();
+        //cons.close();
         return new Videojuego(codigo, titulo, consola, cantJugadores, categoria);
     }
-
+    // Opcion 2
     static public void mostrarVideojuegos(List<Videojuego> videojuegos){
         if(videojuegos.isEmpty()){
             System.out.println("No hay videoujegos cargos. Favor cargue algunos!");
         } else {
-            System.out.println("Test");
+            System.out.printf("| %-6s | %-15s | %-15s | %-13s | %-15s |%n","Codigo","Titulo","Consola","N° Jugadores","Categoria");
+            for(Videojuego vj : videojuegos){
+                System.out.printf("| %06d | %-15s | %-15s | %-13s | %-15s |%n",vj.getCodigo(),vj.getTitulo(),vj.getConsola(),vj.getCantJugadores(),vj.getCategoria());
+            }
         }
     }
-
+    // Opcion 3
+    static public boolean modificarVideojuego(List<Videojuego> videojuegos){
+        boolean encontrado = false;
+        Scanner cons = new Scanner(System.in);
+        if(videojuegos.isEmpty()) {
+            System.out.println("No hay videoujegos cargos. Favor cargue algunos!");
+        } else {
+            System.out.print("Ingrese el codigo a buscar: ");
+            int codigo = cons.nextInt();
+            cons.nextLine();
+            for (Videojuego vj : videojuegos) {
+                if (vj.getCodigo() == codigo) {
+                    System.out.print("Ingrese el titulo: ");
+                    String tit = cons.nextLine();
+                    System.out.print("Ingrese la consola: ");
+                    String con = cons.nextLine();
+                    System.out.print("Ingrese la cantidad de jugadores: ");
+                    int cantJug = cons.nextInt();
+                    cons.nextLine();
+                    System.out.print("Ingrese la categoria: ");
+                    String categoria = cons.nextLine();
+                    vj.setTitulo(tit);
+                    vj.setConsola(con);
+                    vj.setCantJugadores(cantJug);
+                    vj.setCategoria(categoria);
+                    encontrado = true;
+                }
+            }
+        }
+        //cons.close();
+        return encontrado;
+    }
 }
